5179a72f6dd36317cd7f0ec0548638324fd920fc
Санат Байгуаныш <kvenecusghost@gmail.com>
2019-04-03 22:35:24 +0600

    Awesome commit

--- loraClient.go
+++ loraClient.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func main() {
-	fmt.Println("Starting server\n============================")
-	r, err := http.Get("https://ground-sensors-app.data.thethingsnetwork.org/&key=ttn-account-v2.Bf5PmOZpgKj5vzAHdRMgO_PRdRsl-9KPKfP0baapZiU")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Response Header", r.Header)
-	fmt.Println("Response Bpdu", r.Body)
-	//json.Unmarshal(r)
+	http.HandleFunc("/devices", devices)
+	http.ListenAndServe("8080", nil)
+}
+
+func devices(wr http.ResponseWriter, rq *http.Request) {
+	fmt.Println(rq)
+	tpl.Execute(wr, "index.gohtml")
 }
